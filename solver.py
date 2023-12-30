8e297a146e4263b4a57b87892ac2b8931c491791
GowtamGK <garapatigowtam@gmail.com>
2023-12-30 03:42:04 -0800

    updated work

--- solver.py
+++ solver.py
@@ -1,66 +1,78 @@
 # solver.py
+board = [
+                    [3,0,0,8,0,0,0,0,1],
+                    [0,0,0,0,0,2,0,0,0],
+                    [0,4,1,5,0,0,8,3,0],
+                    [0,2,0,0,0,1,0,0,0],
+                    [8,5,0,4,0,3,0,1,7],
+                    [0,0,0,7,0,0,0,2,0],
+                    [0,8,5,0,0,9,7,4,0],
+                    [0,0,0,1,0,0,0,0,0],
+                    [9,0,0,0,0,7,0,0,6]
+]
 
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
+def print_board(brd):
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+   for i in range(len(brd)):
+       if i%3==0 and i !=0:
+           print("- - - - - - - - - - - - ")
 
-            if solve(bo):
-                return True
+       for j in range(len(brd[0])):
+            if j%3==0 and  j!=0:
+                print(" | ", end="")
 
-            bo[row][col] = 0
+            if j == 8:
+                print(brd[i][j])
+            else:
+                print(str(brd[i][j]) + " ", end="")
 
-    return False
+def find_empty(brd):
+     for i in range(len(brd)):
+         for j in range(len(brd[0])):
+             if brd[i][j] == 0:
+                return (i,j)
+     return None
 
-
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+def brd_valid(brd, num, pos):
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1]!=1:
             return False
 
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(brd)):
+        if brd[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+    cube_x = pos[1] // 3
+    cube_y =  pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    for i in range(cube_y*3, cube_y*3 +3):
+        for j in range(cube_x*3, cube_x*3 + 3):
+            if brd[i][j] == num and (i,j) !=pos:
                 return False
 
     return True
 
+def solve_board(brd):
+    #base case
+    empty_spot = find_empty(brd)
+    if not empty_spot:
+        return True
+    else:
+        row, col = empty_spot
 
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+    for i in range(1,10):
+        if brd_valid(brd, i, (row,col)):
+            brd[row][col] = i
 
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+            if solve_board(brd):
+                return True
+            brd[row][col] = 0
 
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
+    return False
 
-
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
-
-    return None
+#displaying the sudoku solver
+print_board(board)
+solve_board(board)
+print("**********************")
+print("***Solved Answer******")
+print_board(board)
